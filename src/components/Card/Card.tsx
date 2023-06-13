052c797c7f7288fb53e5bc106e993013d971750a
Jigang Li <lijigang.us@gmail.com>
2023-06-12 21:24:12 -0400

    Add Card Component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,77 @@
+import React, { ReactNode } from 'react';
+import clsx from 'clsx';
+
+interface CardProp {
+  title: string;
+  subTitle?: string;
+  renderStyle?: 'product' | 'marketing' | 'editorial';
+  children?: ReactNode;
+  footer?: ReactNode;
+  image?: string;
+  imageAlt?: string;
+  imageDirection?: 'vertical' | 'horizontal';
+  size?: 'lg' | 'md' | 'sm';
+}
+
+const Card = ({
+  children,
+  title,
+  subTitle,
+  renderStyle = 'product',
+  imageDirection = 'vertical',
+  size = 'md',
+  footer,
+}: CardProp) => {
+  const containerClasses = clsx('flex border shadow-md', {
+    'flex-col': imageDirection === 'vertical',
+  });
+
+  const bodyClasses = clsx('basis-auto text-sm', {
+    'text-slate-500': renderStyle === 'marketing',
+    'text-sm': size === 'sm',
+    'text-lg': size === 'lg',
+    'text-md': size === 'md',
+  });
+
+  const headerTitleClasses = clsx('text-base', {
+    'text-slate-700': renderStyle === 'marketing',
+    'text-base': renderStyle !== 'editorial',
+    'text-xl': renderStyle === 'editorial',
+  });
+
+  const headerSubtitleClasses = clsx('text-xs', {
+    'text-slate-500': renderStyle === 'marketing',
+  });
+
+  const imageContainerClasses = clsx({});
+
+  const contentContainerClasses = clsx('flex flex-col justify-between', {
+    'p-2 gap-3': renderStyle === 'product',
+    'p-6 gap-6': renderStyle === 'marketing',
+    'p-8 gap-6': renderStyle === 'editorial',
+  });
+
+  return (
+    <div className={containerClasses}>
+      <div className={imageContainerClasses}>
+        <img
+          className="w-full h-full"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/LH_95.jpg/640px-LH_95.jpg"
+          alt="Universe"
+        />
+      </div>
+      <div className={contentContainerClasses}>
+        <div className="basis-1">
+          {subTitle !== '' && (
+            <div className={headerSubtitleClasses}>{subTitle}</div>
+          )}
+          <div className={headerTitleClasses}>{title}</div>
+        </div>
+        <div className={bodyClasses}>{children}</div>
+        <div className="basis-1">{footer}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/Card/stories/Card.stories.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import { Meta } from '@storybook/react';
+import Card from '../Card';
+import { Button } from '../../Button';
+
+export default {
+  title: 'Components/Card',
+  componentl: Card,
+} as Meta;
+
+export const ProductStory = () => {
+  const buttons = <Button>Learn More</Button>;
+  return (
+    <div className="w-72 h-32">
+      <Card title="Card Title" footer={buttons}>
+        Despite a rebound in foot traffic, restaurants and other local shops are
+        struggling to pay the bills.
+      </Card>
+    </div>
+  );
+};
+
+ProductStory.storyName = 'Product Card';
+
+export const MarketingStory = () => {
+  return (
+    <div className="w-72 h-32">
+      <Card title="Card Title" renderStyle="marketing">
+        Despite a rebound in foot traffic, restaurants and other local shops are
+        struggling to pay the bills.
+      </Card>
+    </div>
+  );
+};
+
+ProductStory.storyName = 'Marketing Card';
+
+export const EditorialStory = () => {
+  return (
+    <div className="w-72 h-32">
+      <Card title="Card Title" renderStyle="editorial">
+        Despite a rebound in foot traffic, restaurants and other local shops are
+        struggling to pay the bills.
+      </Card>
+    </div>
+  );
+};
+
+EditorialStory.storyName = 'Editorial Card';
+
+export const ProductHorizontalImageStory = () => {
+  return (
+    <div className="w-[400px] h-32">
+      <Card title="Card Title" imageDirection="horizontal">
+        Despite a rebound in foot traffic, restaurants and other local shops are
+        struggling to pay the bills.
+      </Card>
+    </div>
+  );
+};
+
+ProductHorizontalImageStory.storyName = 'Product Horizontal Image Card';
+
+export const MarketingHorizontalImageStory = () => {
+  return (
+    <div className="w-[400px] h-32">
+      <Card
+        renderStyle="marketing"
+        title="Card Title"
+        imageDirection="horizontal"
+      >
+        Despite a rebound in foot traffic, restaurants and other local shops are
+        struggling to pay the bills.
+      </Card>
+    </div>
+  );
+};
+
+MarketingHorizontalImageStory.storyName = 'Marketing Horizontal Image Card';
+
+export const EditorialHorizontalImageStory = () => {
+  return (
+    <div className="w-[400px] h-32">
+      <Card
+        renderStyle="editorial"
+        title="Card Title"
+        imageDirection="horizontal"
+      >
+        Despite a rebound in foot traffic, restaurants and other local shops are
+        struggling to pay the bills.
+      </Card>
+    </div>
+  );
+};
+
+EditorialHorizontalImageStory.storyName = 'Editorial Horizontal Image Card';
